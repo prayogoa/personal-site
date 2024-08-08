dc6645302e24a06eb79ef7aa5715dbaeadb356a2
Adrian Prayogo <adrian@rainforestqa.com>
2024-08-08 09:55:30 +0800

    fouc fix

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import type { GatsbyBrowser } from "gatsby";
 import { WrapRootElement } from "./src/provider";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({
   element,
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -3,6 +3,9 @@ import type { GatsbySSR } from "gatsby";
 import { ColorModeScript } from "@chakra-ui/react";
 import { WrapRootElement } from "./src/provider";
 import customTheme from "./src/theme";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 export const onRenderBody: GatsbySSR["onRenderBody"] = ({
   setPreBodyComponents,
